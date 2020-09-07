e8ae53842495f960c902c1df10c356544da09168
GCMoura <51333439+GCMoura@users.noreply.github.com>
2020-09-07 20:42:18 -0300

    deploy

--- apagar/app.js
+++
@@ -1,9 +0,0 @@
-var body = document.querySelector('body')
-var div = document.createElement('div')
-
-const parag = document.createElement('p')
-
-parag.appendChild(document.createTextNode("teste"))
-
-div.appendChild(parag)
-body.appendChild(div)
---
+++ server.js
@@ -0,0 +1,12 @@
+const jsonServer = require('json-server')
+const server = jsonServer.create()
+const router = jsonServer.router('src/data/db.json')
+const middlewares = jsonServer.defaults()
+
+const port = process.env.PORT || 3333
+
+server.use(middlewares)
+server.use(router)
+server.listen(port, () => {
+    console.log(`JSON server is running in port ${port}`)
+})
